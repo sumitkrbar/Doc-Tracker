d3603cd358888d5e0f239c453560eeea59d6bd08
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-11-03 22:20:23 +0530

    updated user schema

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+  password:{
+        type: String,
+        required: true,
+},
   // storing reference to documents also if needed
   documents: [
     {
@@ -17,6 +21,6 @@ const userSchema = new mongoose.Schema({
       ref: "Document"
     }
   ]
-});
+},{timestamps: true});
 
 export default mongoose.model("User", userSchema);
