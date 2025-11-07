2284c0721795db998a32c90923154ed526280bb7
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-11-07 21:12:32 +0530

    migrate: nodemailer to resend

--- server/services/mailService.js
+++ server/services/mailService.js
@@ -1,7 +1,33 @@
-import {accountVerificationMail} from "../utils/mailObject.js";
-import { transporter } from "../config/mail.js";
+import { accountVerificationMail } from "../utils/mailObject.js";
+//import { transporter } from "../config/mail.js";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendAccountVerificationMail = async (to, token) => {
-    const mailOptions = accountVerificationMail(to, token);
-    await transporter.sendMail(mailOptions);
-}
+  const mailOptions = accountVerificationMail(to, token);
+
+  try {
+    // 1) Send through Nodemailer
+    //await transporter.sendMail(mailOptions);
+
+    // 2) Send through Resend (check result)
+    // send otp to admin mail 
+    const { data, error } = await resend.emails.send({
+        from: "lekha <onboarding@resend.dev>",
+        to: process.env.MAIL_ID,
+        subject: mailOptions.subject,
+        text: mailOptions.text,
+        html: mailOptions.html,
+    });
+
+    if (error) {
+      console.error("Resend error:", error);
+      throw new Error("Failed to send verification email via Resend");
+    }
+
+  } catch (error) {
+    console.error("Error sending verification emails:", error);
+    throw new Error("Failed to send verification email");
+  }
+};
--- server/utils/mailObject.js
+++ server/utils/mailObject.js
@@ -11,7 +11,7 @@ Your OTP for account verification is: ${token}
 If you didn't request this, simply ignore this mail.`,
   html: `
     <p>Welcome to <strong>Lekha</strong> 🚀</p>
-    <p>Your OTP for account verification is:</p>
+    <p>Your OTP for account (${to}) verification is:</p>
     <h2 style="margin: 10px 0; font-size: 24px;">${token}</h2>
     <p>If you didn't request this, please ignore this email.</p>
   `
