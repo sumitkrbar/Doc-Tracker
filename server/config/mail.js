6ef779722fbb3ab046d5b167d02368bd22a5f4dd
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-11-07 09:36:22 +0530

    Updated mail.js

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,9 +1,12 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,           // Must be 465 for Render
+  secure: true,        // Must be true for port 465
   auth: {
-    user: process.env.MAIL_ID,  // your gmail
-    pass: process.env.MAIL_PASSWORD,  // your app password
-  }
+    user: process.env.MAIL_ID,       // your Gmail
+    pass: process.env.MAIL_PASSWORD,       // your App Password
+  },
 });
+
